8b000b593fbc6bb655d1c73e2c2ed3e592ef3bac
Karen Payne <paynekaren@comcast.net>
2020-05-05 14:04:19 -0700

    Added projections

---
+++ IncludeFilter/Projections/ContactTypeItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncOperations.Projections
+{
+    public class ContactTypeItem
+    {
+        public int ContactTypeIdentifier { get; set; }
+        public string ContactTitle { get; set; }
+
+        public override string ToString() => ContactTitle;
+        /// <summary>
+        /// This projection simplifies a lambda select
+        /// </summary>
+        public static Expression<Func<ContactType, ContactTypeItem>> Projection =>
+            contactType => new ContactTypeItem()
+            {
+                ContactTypeIdentifier = contactType.ContactTypeIdentifier,
+                ContactTitle = contactType.ContactTitle
+
+            };
+    }
+}
---
+++ IncludeFilter/Projections/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncOperations.Projections
+{
+    class Country
+    {
+        public int CountryIdentifier { get; set; }
+        public string Name { get; set; }
+        public override string ToString() => Name;
+        /// <summary>
+        /// This projection simplifies a lambda select
+        /// </summary>
+        public static Expression<Func<Countries, Country>> Projection =>
+            country => new Country()
+            {
+                CountryIdentifier = country.CountryIdentifier,
+                Name = country.Name
+            };
+    }
+}
